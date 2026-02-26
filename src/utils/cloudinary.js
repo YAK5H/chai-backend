ed583d8dc221853f648a302eaf7ff92e5786f93f
Yaksh raj Banote <banoteyakshraj@gmail.com>
2026-02-26 22:59:35 +0530

    postman check and bug fixes

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -5,7 +5,7 @@ import fs from "fs"
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_Key: process.env.CLOUDINARY_API_KEY,
+    api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
@@ -17,8 +17,9 @@ const uploadOnCloudinary = async (localFilePath) =>{
             resource_type: "auto"
         })
         // file has been uploaded successfully
-        console.log("file has been uploaded in cloudinary"
-        ,response.url);
+        // console.log("file has been uploaded in cloudinary"
+        // ,response.url);
+        fs.unlinkSync(localFilePath)
         return response;
     } catch(error){
         fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation failed
